7450a7e99f88e8d2e7625b3c24416efc196b334f
Omar Guerrero <guerrero9725@gmail.com>
2017-12-04 11:18:47 -0500

    CRUD finalizado

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require('express'),
     engines = require('consolidate'),
     MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
+    assert = require('assert'),
+    app = express();;
 
-var app = express();
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
+
+app.use(express.json());
+app.use(express.urlencoded());
 
 MongoClient.connect('mongodb://localhost:27017/quiz', function (err, db) {
     if (err) throw err;
@@ -19,8 +22,66 @@ MongoClient.connect('mongodb://localhost:27017/quiz', function (err, db) {
         });
     });
 
-    app.get('/new', function (err, res) {
+    app.get('/movies/new', function (req, res) {
         res.render('new', {});
+    });
+
+    app.post('/movies', function (req, res) {
+        var movie = {
+            title: req.body.title, 
+            year: parseInt(req.body.year),
+            imdb: req.body.imdb
+        }
+
+        db.collection('movies').insertOne(movie, function (err, result) {
+            if (err) throw err
+            res.render('show', {message: 'Movie added saccessfully', movie: movie});
+        })
+    });
+
+    app.get('/movies/:title', function (req, res) {
+        var title = req.params.title;
+
+        db.collection('movies').find({title: title}).toArray(function (err, result) {
+            var movie = result[0];
+            res.render('edit', {movie: movie});
+        })
+    });
+
+    app.post('/movies/:title', function (req, res) {
+        var title = req.params.title,
+            ntitle = req.body.title,
+            nyear = parseInt(req.body.year),
+            nimdb = req.body.imdb;
+
+        db.collection('movies').findAndModify(
+            {title: title},
+            [['_id', 'asc']],
+            {
+                $set: {
+                    title: ntitle,
+                    year: nyear,
+                    imdb: nimdb
+                },
+                $currentDate: {
+                    lastModified: true
+                }
+            },
+            function (err, result) {
+                console.log(result)
+                res.render('show', {message: 'Movie update succesfully', movie: result.value})
+            }
+        )
+    })
+
+    app.get('/movies/delete/:title', function (req, res) {
+        var title = req.params.title;
+
+        db.collection('movies').deleteOne({
+            title: title
+        }, function (result) {
+            res.redirect('/');
+        })
     });
 
     app.use(function(req, res){
